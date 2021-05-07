79238ff5fcf9f61015928af482839d51d9cbe0f0
cypherbits <info@avanix.es>
2021-05-07 22:11:50 +0200

    Remove many duplicated code and make video work.
    Signed-off-by: cypherbits <info@avanix.es>

--- public/js/omeglo.js
+++ public/js/omeglo.js
@@ -3,7 +3,7 @@
 let URLConnection = "omeglit.com";
 let URLProtocol = "https://";
 
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1"){
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     URLConnection = "localhost:8080";
     URLProtocol = "http://";
 }
@@ -63,8 +63,7 @@ $(document).ready(function () {
             isText = false;
             isVideo = true;
             is18 = false;
-            prepareCamera(false);
-            //prepareVideoChat(false);
+            prepareCamera();
         });
     });
 
@@ -74,8 +73,7 @@ $(document).ready(function () {
             isText = false;
             isVideo = true;
             is18 = true;
-            prepareCamera(true);
-            //prepareVideoChat(true);
+            prepareCamera();
         });
     });
 
@@ -86,9 +84,33 @@ $(document).ready(function () {
 });
 
 
-function prepareVideoChat(is18) {
+const chatLog = {
+    clear: function () {
+        $("#txtChatLog").html("");
+    },
+    addSystemMessage: function (data) {
+        $("#txtChatLog").append('<div class="item">' + data + '</div>');
+        updateScroll();
+    },
+    addStrangerMessage: function (data) {
+        const encodedStr = data.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+            return '&#' + i.charCodeAt(0) + ';';
+        });
+        $("#txtChatLog").append('<div class="item"><span class="stranger">Stranger: </span><span class="msg">' + encodedStr + '</span></div>');
+        updateScroll();
+    },
+    addMeMessage: function (data) {
+        const encodedStr = data.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+            return '&#' + i.charCodeAt(0) + ';';
+        });
+        $("#txtChatLog").append('<div class="item"><span class="you">You: </span><span class="msg">' + encodedStr + '</span></div>');
+        updateScroll();
+    }
+};
 
-//    var videoTracks = localStream.getVideoTracks();
+function prepareChat() {
+
+    //    var videoTracks = localStream.getVideoTracks();
 //    var audioTracks = localStream.getAudioTracks();
 //    if (videoTracks.length > 0) {
 //        trace('Using video device: ' + videoTracks[0].label);
@@ -96,231 +118,6 @@ function prepareVideoChat(is18) {
 //    if (audioTracks.length > 0) {
 //        trace('Using audio device: ' + audioTracks[0].label);
 //    }
-
-    $("#btnNewChat").prop("disabled", true);
-    $("#btnSendMessage").prop("disabled", true);
-
-    $('#txtNewMessage').unbind().keypress(function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode === 13) {
-            $('#btnSendMessage').click();
-            return false;
-        }
-    });
-
-    chatLog.clear();
-    chatLog.addSystemMessage("Looking for a partner...");
-
-    var localConnection = new RTCPeerConnection(iceServers);
-
-    localConnection.onicecandidate = onICECandidate;
-    localConnection.ondatachannel = receiveChannelCallback;
-    localConnection.onaddstream = receiveStreamCallback;
-
-    var sendChannel = null;
-
-    if (is18) {
-        socketControl = io.connect(URLProtocol + URLConnection + '/video18');
-    } else {
-        socketControl = io.connect(URLProtocol + URLConnection + '/video');
-    }
-
-
-    socketControl.emit('newUser', {});
-
-    socketControl.on("match", function (data) {
-        console.log("creating offer");
-        chatLog.addSystemMessage("Partner found, trying to connect...");
-
-        if (data.itsok) {
-            sendChannel = localConnection.createDataChannel("sendChannel");
-            sendChannel.onopen = onSendChannelStateChange;
-            sendChannel.onclose = onSendChannelStateChange;
-            //Chrome workaround
-            sendChannel.onclosing = onSendChannelStateChange;
-            sendChannel.onmessage = handleReceiveMessage;
-
-            localConnection.addStream(localStream);
-
-            localConnection.createOffer({
-                audio: true,
-                video: true
-            }).then(
-                    gotDescription,
-                    onCreateSessionDescriptionError
-                    );
-        }
-
-    });
-
-
-  /*  socketControl.on("disconnect", function (data) {
-
-        chatLog.addSystemMessage("Stranger have disconnected.");
-        disconnect();
-
-    });*/
-
-    socketControl.on("newMessage", function (data) {
-        switch (data.type) {
-            case "new-offer":
-
-                localConnection.addStream(localStream);
-
-                localConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(data.msg)), function () {
-                    localConnection.createAnswer().then(
-                            gotAnswer,
-                            onCreateAnswerError
-                            );
-                }, function (e) {
-                    console.error("ERROR" + e);
-                });
-
-                console.debug("we got new remote offer: " + JSON.parse(data.msg));
-                break;
-            case "new-answer":
-                localConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(data.msg)));
-                console.debug("we got new remote answer: " + JSON.parse(data.msg));
-                break;
-            case "new-ice":
-                if (JSON.parse(data.msg) != null) {
-                    localConnection.addIceCandidate(new RTCIceCandidate(JSON.parse(data.msg)));
-                }
-
-                console.debug("we got new remote ICE candidate: " + JSON.parse(data.msg));
-                break;
-        }
-    });
-
-    function receiveStreamCallback(e) {
-        //remoteVideo.srcObject = e.stream;
-        //$('#remoteVideo').prop('src', URL.createObjectURL(e.stream));
-        var remoteVideo = document.querySelector('#remoteVideo');
-        remoteVideo.srcObject = e.stream;
-
-        //console.log("remote video stream add");
-
-        resizeVideos();
-    }
-
-    function receiveChannelCallback(event) {
-        console.log("channel received");
-
-        sendChannel = event.channel;
-
-        sendChannel.onopen = onSendChannelStateChange;
-        sendChannel.onclose = onSendChannelStateChange;
-        //Chrome workaround
-        sendChannel.onclosing = onSendChannelStateChange;
-        sendChannel.onmessage = handleReceiveMessage;
-    }
-
-    function handleReceiveMessage(event) {
-        //console.log("new MESSAGE: " + event.data);
-        chatLog.addStrangerMessage(event.data);
-    }
-
-    function onICECandidate(ice) {
-        console.dir("sending new ICE candidate: " + ice.candidate);
-        socketControl.emit('newMessage', {
-            type: 'new-ice',
-            msg: JSON.stringify(ice.candidate)
-        });
-    }
-
-    function onCreateSessionDescriptionError(error) {
-        console.error('Failed to create session description: ' + error.toString());
-    }
-
-    function onCreateAnswerError(error) {
-        console.error('Failed to create session answer: ' + error.toString());
-    }
-
-    function gotAnswer(data) {
-        console.debug("sending answer: " + data);
-        localConnection.setLocalDescription(data);
-        socketControl.emit('newMessage', {
-            type: 'new-answer',
-            msg: JSON.stringify(data)
-        });
-    }
-
-    function gotDescription(desc) {
-        localConnection.setLocalDescription(desc);
-        console.debug('sending offer ' + desc.sdp);
-
-        socketControl.emit('newMessage', {
-            type: 'new-offer',
-            msg: JSON.stringify(desc)
-        });
-    }
-
-    function onSendChannelStateChange() {
-        var readyState = sendChannel.readyState;
-        console.log('Send channel state is: ' + readyState);
-        if (readyState === 'open') {
-            //habilitar botones para enviar
-            chatLog.clear();
-            chatLog.addSystemMessage("You're now chatting with a random stranger. Say hi!");
-
-            $("#btnNewChat").html("Disconnect");
-            $("#btnNewChat").unbind().on("click", function () {
-                chatLog.addSystemMessage("You have disconnected.");
-                disconnect();
-            });
-            $("#btnNewChat").prop("disabled", false);
-            $("#btnSendMessage").prop("disabled", false);
-            $("#txtNewMessage").prop("disabled", false);
-        } else {
-            //deshabilitar botones para enviar
-            console.log("not data channel open");
-            chatLog.addSystemMessage("Stranger have disconnected.");
-            $("#btnNewChat").prop("disabled", false);
-            disconnect();
-
-        }
-    }
-
-    $("#btnSendMessage").unbind().on("click", function () {
-        sendMessage();
-    });
-
-    function sendMessage() {
-        var msg = $("#txtNewMessage").val();
-
-        if (msg.trim() != "") {
-            sendChannel.send(msg.trim());
-
-            $("#txtNewMessage").val("");
-
-            chatLog.addMeMessage(msg);
-        }
-
-    }
-
-    function disconnect() {
-
-        sendChannel.close();
-
-        localConnection.close();
-
-        sendChannel = null;
-        localConnection = null;
-
-        socketControl.close();
-
-        $("#btnNewChat").html("New chat");
-        $("#btnNewChat").unbind().on("click", function () {
-            prepareChat(false);
-        });
-        $("#btnSendMessage").prop("disabled", true);
-        $("#txtNewMessage").prop("disabled", true);
-    }
-
-}
-
-
-function prepareChat() {
 
     if (is18) {
         $("#btnNewChat").removeClass("btn-primary").addClass("btn-danger");
@@ -346,15 +143,25 @@ function prepareChat() {
 
     localConnection.onicecandidate = onICECandidate;
     localConnection.ondatachannel = receiveChannelCallback;
+    if (isVideo) {
+        localConnection.onaddstream = receiveStreamCallback;
+    }
 
     let sendChannel = null;
 
     if (is18) {
-        socketControl = io.connect(URLProtocol + URLConnection + '/txt18');
+        if (isVideo) {
+            socketControl = io.connect(URLProtocol + URLConnection + '/video18');
+        } else {
+            socketControl = io.connect(URLProtocol + URLConnection + '/txt18');
+        }
     } else {
-        socketControl = io.connect(URLProtocol + URLConnection + '/txt');
+        if (isVideo) {
+            socketControl = io.connect(URLProtocol + URLConnection + '/video');
+        } else {
+            socketControl = io.connect(URLProtocol + URLConnection + '/txt');
+        }
     }
-
 
     socketControl.emit('newUser', {});
 
@@ -370,30 +177,42 @@ function prepareChat() {
             sendChannel.onclosing = onSendChannelStateChange;
             sendChannel.onmessage = handleReceiveMessage;
 
-            localConnection.createOffer().then(
-                    gotDescription,
-                    onCreateSessionDescriptionError
-                    );
+            let options = {};
+            if (isVideo) {
+                localConnection.addStream(localStream);
+                options = {
+                    audio: true,
+                    video: true
+                };
+            }
+
+            localConnection.createOffer(options).then(
+                gotDescription,
+                onCreateSessionDescriptionError
+            );
         }
 
     });
 
 
-  /*  socketControl.on("disconnect", function (data) {
+    /*  socketControl.on("disconnect", function (data) {
 
-        chatLog.addSystemMessage("Stranger have disconnected.");
-        disconnect();
+          chatLog.addSystemMessage("Stranger have disconnected.");
+          disconnect();
 
-    });*/
+      });*/
 
     socketControl.on("newMessage", function (data) {
         switch (data.type) {
             case "new-offer":
+                if (isVideo) {
+                    localConnection.addStream(localStream);
+                }
                 localConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(data.msg)), function () {
                     localConnection.createAnswer().then(
-                            gotAnswer,
-                            onCreateAnswerError
-                            );
+                        gotAnswer,
+                        onCreateAnswerError
+                    );
                 }, function (e) {
                     console.error("ERROR" + e);
                 });
@@ -413,6 +232,17 @@ function prepareChat() {
                 break;
         }
     });
+
+    function receiveStreamCallback(e) {
+        //remoteVideo.srcObject = e.stream;
+        //$('#remoteVideo').prop('src', URL.createObjectURL(e.stream));
+        const remoteVideo = document.querySelector('#remoteVideo');
+        remoteVideo.srcObject = e.stream;
+
+        //console.log("remote video stream add");
+
+        //resizeVideos();
+    }
 
     function receiveChannelCallback(event) {
         console.log("channel received");
@@ -467,7 +297,7 @@ function prepareChat() {
     }
 
     function onSendChannelStateChange() {
-        var readyState = sendChannel.readyState;
+        const readyState = sendChannel.readyState;
         console.log('Send channel state is: ' + readyState);
         if (readyState === 'open') {
             //habilitar botones para enviar
@@ -497,7 +327,7 @@ function prepareChat() {
     });
 
     function sendMessage() {
-        var msg = $("#txtNewMessage").val();
+        const msg = $("#txtNewMessage").val();
 
         if (msg.trim() !== "") {
             sendChannel.send(msg.trim());
@@ -531,7 +361,7 @@ function prepareChat() {
 }
 
 
-function prepareCamera(is18) {
+function prepareCamera() {
 
     $(window).resize(function () {
         //resizeCamera();
@@ -549,12 +379,6 @@ function prepareCamera(is18) {
 
     //resizeCamera();
 
-    if (is18) {
-        $("#btnNewChat").removeClass("btn-primary").addClass("btn-danger");
-        $("#btnSendMessage").removeClass("btn-primary").addClass("btn-danger");
-        $("body").addClass("text-danger");
-    }
-
     chatLog.clear();
     chatLog.addSystemMessage("Enable access to your webcam");
 
@@ -563,12 +387,12 @@ function prepareCamera(is18) {
         // Set your video displays
 
         //$('#localVideo').prop('src', window.URL.createObjectURL(stream));
-        var localVideo = document.querySelector('#localVideo');
+        const localVideo = document.querySelector('#localVideo');
         localVideo.srcObject = stream;
 
         localStream = stream;
 
-        prepareVideoChat(is18);
+        prepareChat();
 
     }, function (error) {
         console.error(error);
@@ -591,32 +415,8 @@ function trace(text) {
 
 }
 
-var chatLog = {
-    clear: function () {
-        $("#txtChatLog").html("");
-    },
-    addSystemMessage: function (data) {
-        $("#txtChatLog").append('<div class="item">' + data + '</div>');
-        updateScroll();
-    },
-    addStrangerMessage: function (data) {
-        var encodedStr = data.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
-            return '&#' + i.charCodeAt(0) + ';';
-        });
-        $("#txtChatLog").append('<div class="item"><span class="stranger">Stranger: </span><span class="msg">' + encodedStr + '</span></div>');
-        updateScroll();
-    },
-    addMeMessage: function (data) {
-        var encodedStr = data.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
-            return '&#' + i.charCodeAt(0) + ';';
-        });
-        $("#txtChatLog").append('<div class="item"><span class="you">You: </span><span class="msg">' + encodedStr + '</span></div>');
-        updateScroll();
-    }
-};
-
 function updateScroll() {
-    var element = document.getElementById("txtChatLog");
+    const element = document.getElementById("txtChatLog");
     element.scrollTop = element.scrollHeight;
 }
 
@@ -625,10 +425,10 @@ function isBreakpoint(alias) {
 }
 
 $(window).resize(function () {
-    if (isVideo){
+    if (isVideo) {
         //resizeVideos();
     }
-    if (isVideo || isText){
+    if (isVideo || isText) {
         updateScroll();
     }
 });
