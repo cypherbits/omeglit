ff2304575a4d1a82c5fcc547ac9ec6a8b0d4bcb6
cypherbits <info@avanix.es>
2021-05-09 16:43:28 +0200

    wip
    Signed-off-by: cypherbits <info@avanix.es>

--- public/js/omeglo.js
+++ public/js/omeglo.js
@@ -422,7 +422,7 @@ function trace(text) {
         text = text.substring(0, text.length - 1);
     }
     if (window.performance) {
-        var now = (window.performance.now() / 1000).toFixed(3);
+        const now = (window.performance.now() / 1000).toFixed(3);
         console.log(now + ': ' + text);
     } else {
         console.log(text);
